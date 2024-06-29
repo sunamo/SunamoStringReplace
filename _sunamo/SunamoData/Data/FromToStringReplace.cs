be6875f3a6ec13382d1c3ed86a9968b316b52179
sunamo.cz_asus <radek.jancik@sunamo.cz>
2024-06-29 11:37:51 +0200

    Make some classes public when it is needed

---
+++ _sunamo/SunamoData/Data/FromToStringReplace.cs
@@ -0,0 +1,35 @@
+namespace SunamoStringReplace;
+
+
+/// <summary>
+///     Must have always entered both from and to
+///     None of event could have unlimited time!
+/// </summary>
+internal class FromToStringReplace : FromToTSHStringReplace<long>
+{
+    internal static FromToStringReplace Empty = new(true);
+    internal FromToStringReplace()
+    {
+    }
+    /// <summary>
+    ///     Use Empty contstant outside of class
+    /// </summary>
+    /// <param name="empty"></param>
+    private FromToStringReplace(bool empty)
+    {
+        this.empty = empty;
+    }
+    /// <summary>
+    ///     A3 true = DateTime
+    ///     A3 False = None
+    /// </summary>
+    /// <param name="from"></param>
+    /// <param name="to"></param>
+    /// <param name="ftUse"></param>
+    internal FromToStringReplace(long from, long to, FromToUse ftUse = FromToUse.DateTime)
+    {
+        this.from = from;
+        this.to = to;
+        this.ftUse = ftUse;
+    }
+}
---
+++ _sunamo/SunamoData/Data/FromToTSHStringReplace.cs
@@ -0,0 +1,49 @@
+namespace SunamoStringReplace;
+
+
+internal class FromToTSHStringReplace<T>
+{
+    
+    internal bool empty;
+    protected long fromL;
+    internal FromToUse ftUse = FromToUse.DateTime;
+    protected long toL;
+    internal FromToTSHStringReplace()
+    {
+        var t = typeof(T);
+        if (t == Types.tInt) ftUse = FromToUse.None;
+    }
+    /// <summary>
+    ///     Use Empty contstant outside of class
+    /// </summary>
+    /// <param name="empty"></param>
+    private FromToTSHStringReplace(bool empty) : this()
+    {
+        this.empty = empty;
+    }
+    /// <summary>
+    ///     A3 true = DateTime
+    ///     A3 False = None
+    /// </summary>
+    /// <param name="from"></param>
+    /// <param name="to"></param>
+    /// <param name="ftUse"></param>
+    internal FromToTSHStringReplace(T from, T to, FromToUse ftUse = FromToUse.DateTime) : this()
+    {
+        this.from = from;
+        this.to = to;
+        this.ftUse = ftUse;
+    }
+    internal T from
+    {
+        get => (T)(dynamic)fromL;
+        set => fromL = (long)(dynamic)value;
+    }
+    internal T to
+    {
+        get => (T)(dynamic)toL;
+        set => toL = (long)(dynamic)value;
+    }
+    internal long FromL => fromL;
+    internal long ToL => toL;
+}
